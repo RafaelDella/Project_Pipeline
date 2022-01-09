8bcb202a090bcb3d565ee916b404a80e6ab5b095
Rafael Della Giustina <rafael.della64@gmail.com>
2022-01-09 12:05:05 -0300

    Start Connection pt4
    Joining The Created Room
    Handling Creation Errors

--- Project Pipeline/Assets/Scripts/Launcher.cs
+++ Project Pipeline/Assets/Scripts/Launcher.cs
@@ -21,6 +21,12 @@ public class Launcher : MonoBehaviourPunCallbacks
     public GameObject createRoomScreen;
     public TMP_InputField roomNameInput;
 
+    public GameObject roomScreen;
+    public TMP_Text roomNameText;
+
+    public GameObject errorScreen;
+    public TMP_Text errorText;
+
     private void Start() {
         CloseMenus();
 
@@ -34,6 +40,8 @@ public class Launcher : MonoBehaviourPunCallbacks
         loadingScreen.SetActive(false);
         menuButtons.SetActive(false);
         createRoomScreen.SetActive(false);
+        roomScreen.SetActive(false);
+        errorScreen.SetActive(false);
     }
 
     public override void OnConnectedToMaster(){
@@ -55,9 +63,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
 
     public void CreateRoom(){
-        if(!string.isNullOrEmpty(roomNameInput.text)){
+        if(!string.IsNullOrEmpty(roomNameInput.text)){
             RoomOptions options = new RoomOptions();
-            options.maxPlayer = 8;
+            options.MaxPlayers = 8;
 
             PhotonNetwork.CreateRoom(roomNameInput.text, options);
 
@@ -65,6 +73,36 @@ public class Launcher : MonoBehaviourPunCallbacks
             loadingText.text = "Creating Room...";
             loadingScreen.SetActive(true);
         }
+    }
+
+    public override void OnJoinedRoom(){
+        CloseMenus();
+        roomScreen.SetActive(true);
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message){
+        errorText.text = "Failed To Created Room: " + message;
+        CloseMenus();
+        errorScreen.SetActive(true);
+
+    }
+
+    public void CloseErrorScreen(){
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    public void LeaveRoom(){
+        PhotonNetwork.LeaveRoom();
+        CloseMenus();
+        loadingText.text = "Leaving Room...";
+        loadingScreen.SetActive(true);
+    }
+
+    public override void OnLeftRoom(){
+        CloseMenus();
+        menuButtons.SetActive(true);
     }
 
 }
