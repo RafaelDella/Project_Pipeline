c7da25c21d07bb4184d7b22c017af0975e542f0e
Rafael Della Giustina <rafael.della64@gmail.com>
2022-01-11 20:21:18 -0300

    set up events pt2

--- Project Pipeline/Assets/Scripts/MatchManager.cs
+++ Project Pipeline/Assets/Scripts/MatchManager.cs
@@ -14,13 +14,32 @@ public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
         instance = this;
     }
 
+    public enum EventCodes : byte{ //enum a specific category of data, easy to use
+        NewPlayer,
+        ListPlayer,
+        ChangeStat,
+    }
+
+    public List<PlayerInfo> allPlayers = new List<PlayerInfo>();
+    private int index;
+
+    public EventCodes theEvent;
+
     private void Start() {
         if(!PhotonNetwork.IsConnected){
             SceneManager.LoadScene(0); //Main Menu
         }
     }
 
-    public oid OnEvent(EventData photonEvent){
+    private void Update() {
+        //PlayerInfo p1 = new PlayerInfo("Rafael", 0, 8, 5);
+
+
+
+
+    }
+
+    public void OnEvent(EventData photonEvent){
 
     }
 
@@ -31,4 +50,20 @@ public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
     public override void OnDisable(){
         PhotonNetwork.RemoveCallbackTarget(this);
     }
+}
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int actor, kills, death;
+
+    public PlayerInfo(string _name, int _actor, int _kilss, int _death){
+        name = _name;
+        actor = _actor;
+        kills = _kilss;
+        death = _death;
+    }
+
+
 }
