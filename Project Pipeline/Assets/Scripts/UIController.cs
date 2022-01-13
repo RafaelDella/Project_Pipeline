43a082b16ad666990364b9db033d62adfcea1250
Rafael Della Giustina <rafael.della64@gmail.com>
2022-01-13 15:59:20 -0300

    display stats on screen

--- Project Pipeline/Assets/Scripts/UIController.cs
+++ Project Pipeline/Assets/Scripts/UIController.cs
@@ -16,6 +16,8 @@ public class UIController : MonoBehaviour
 
     public Slider healthSlider;
 
+    public TMP_Text killsText, deathsText;
+
     private void Awake() {
         instance = this;
     }
